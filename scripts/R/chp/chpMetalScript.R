80320dfd6d5ba6536ce6bb54a62f713b95fd9f28
Marijana Vujkovic <vujkovicm@users.noreply.github.com>
2017-09-13 17:47:25 -0400

    Create chpMetalScript.R

---
+++ scripts/R/chp/chpMetalScript.R
@@ -0,0 +1,37 @@
+args = commandArgs(trailingOnly = TRUE)
+dir  = "/group/research/mvp001/snakemake/"
+
+# create metal script file
+sink(file = args[1], append = T)
+
+# add metal parameters
+cat("SCHEME STDERR\n")
+cat("CUSTOMVARIABLE N_INFORMATIVE\n")
+cat("LABEL N_INFORMATIVE AS N_INFORMATIVE\n\n")
+cat("MARKER CHRCBP\n")
+cat("#FREQ AF\n")
+cat("ALLELE REF ALT\n")
+cat("EFFECT EFFECT\n")
+cat("PVALUE PVALUE\n")
+cat("WEIGHTLABEL N_INFORMATIVE\n")
+cat("STDERR SE\n\n")
+cat("AVERAGEFREQ ON\n")
+cat("MINMAXFREQ ON\n")
+cat("COLUMNCOUNTING STRICT\n")
+cat("GENOMICCONTROL ON\n")
+cat("SEPERATOR WHITESPACE\n")
+
+# meta-analysis files
+cat(paste("PROCESS ", dir, args[2], "\n", sep = ""))
+cat(paste("PROCESS ", dir, args[3], "\n", sep = ""))
+cat(paste("PROCESS ", dir, args[4], "\n", sep = ""))
+cat("\n")
+
+# export output
+cat(paste("OUTFILE ", dir, args[5], "\n", sep = ""))
+cat("ANALYZE HETEROGENEITY\n")
+cat("QUIT\n")
+
+# close metal script file
+sink()
+
