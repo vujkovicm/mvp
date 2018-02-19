54b50ceb6f219df2be7fba793484f1cf9d341f5c
Marijana Vujkovic <vujkovicm@users.noreply.github.com>
2018-02-19 10:39:25 -0500

    Update impLocusPrep.R

--- scripts/R/imp/impLocusPrep.R
+++ scripts/R/imp/impLocusPrep.R
@@ -1,33 +1,36 @@
 args = commandArgs(trailingOnly = T)
 
-pval_cut        <- 5e-8
-half_window     <- 500000       ### 500Kb, half window
+pval_cut        = 5e-8
+half_window     = 500000       ### 500Kb, half window
 
 file.in    <- args[1]
 file.out   <- args[2]
 
-d       <- read.table(file = file.in, header = T, as.is = T, sep = "\t")
+d     = read.table(file = file.in, header = T, as.is = T, sep = "\t")
+d$CHR = gsub(pattern = "(:).*", replacement = "", x = d$CHRCBP)
+d$CHR = gsub(pattern = "chr", replacement = "", x = d$CHR)
+d$POS = as.numeric(gsub(pattern = ".*(:)", replacement = "", x = d$CHRCBP))
 
-### provide the chromosome and position information
-pos     <- regexpr(":", d$CHRCBP, fixed=TRUE)
-d$chr   <- as.numeric(substr(d$CHRCBP, 4, pos - 1))
-d$pos   <- as.numeric(substr(d$CHRCBP, pos + 1, nchar(d$CHRCBP)))
-combo   <- NULL
-for (iCHR in 1:22) {
+comb    <- NULL
+for (iCHR in 1:22) 
+  {
   ### lead SNP candidates
-  ds      <- d[d$chr == iCHR & d$PVALUE < pval_cut, c("pos", "PVALUE")]
-  if (nrow(ds) > 0) {
-    for (i in 1:nrow(ds)) {
-        current_pos     <- ds$pos[i]
+  ds      <- d[d$CHR == iCHR & d$PVALUE < pval_cut, c("POS", "PVALUE")]
+  if (nrow(ds) > 0) 
+  {
+    for (i in 1:nrow(ds)) 
+    {
+        current_pos     <- ds$POS[i]
         current_pval    <- ds$PVALUE[i]
-        current_nbhd    <- ds[ds$pos >= current_pos - half_window & ds$pos <= current_pos + half_window, ]
+        current_nbhd    <- ds[ds$POS >= current_pos - half_window & ds$POS <= current_pos + half_window,]
         most_sig_pval   <- min(current_nbhd$PVALUE)
-        refsnp_pos      <- current_nbhd$pos[current_nbhd$PVALUE == most_sig_pval]
+        refsnp_pos      <- current_nbhd$POS[current_nbhd$PVALUE == most_sig_pval]
         refsnp_chrcbp   <- paste0("chr", iCHR, ":", refsnp_pos)
         df      <- data.frame(chr = iCHR, pos = refsnp_pos, chrcbp = refsnp_chrcbp, pval = most_sig_pval)
-        combo   <- rbind(combo, df)
-    } 
+        comb    <- rbind(comb, df)
+    }
   }
-} 
-combo    <- comb[!duplicated(combo), ]
-write.table(combo, file = file.out, col.names = T, row.names = F, quote = F, sep = "\t")
+}
+
+comb    <- comb[!duplicated(comb), ]
+write.table(comb, file = file.out, col.names = T, row.names = F, quote = F, sep = "\t")
