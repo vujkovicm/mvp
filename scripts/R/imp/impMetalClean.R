c5c2feca438d347db7abaee6de692ad17207d121
Marijana Vujkovic <vujkovicm@users.noreply.github.com>
2018-02-19 10:35:25 -0500

    Update impMetalClean.R

--- scripts/R/imp/impMetalClean.R
+++ scripts/R/imp/impMetalClean.R
@@ -8,21 +8,34 @@ file.sig = args[3]
 file.tab = args[4] 
 
 # import
-d <- read.table(file = file.in, header = T, as.is = T, sep = " ", stringsAsFactors = F)
-colnames(d)[1] = "CHRCBP"
-colnames(d)[3] = "POS"
-colnames(d)[6] = "N_INFORMATIVE"
-colnames(d)[8] = "EFFECT"
-colnames(d)[10] = "PVALUE"
+d <- read.table(file = file.in, header = T, as.is = T, sep = "\t", stringsAsFactors = F)
 
-d.all = d[, c("CHRCBP", "PVALUE")]
-d.sig = subset(d.all, d.all$PVALUE < 5e-08)
+# INSERT CHR AND BP
+colnames(d)[1]  = "CHRCBP"
+colnames(d)[16] = "N_INFORMATIVE"
+colnames(d)[8]  = "EFFECT"
+colnames(d)[10] = "PVALUE"
+d$EA            = toupper(d$Allele1)
+d$NEA           = toupper(d$Allele2)
+colnames(d)[4]  = "EAF"
+colnames(d)[9]  = "SE"
+
+# remove everything after : (CHR) or everything before : (BP)
+d$CHR = gsub(pattern = "(:).*", replacement = "", x = d$CHRCBP)
+d$CHR = gsub(pattern = "chr", replacement = "", x = d$CHR)
+d$POS = gsub(pattern = ".*(:)", replacement = "", x = d$CHRCBP)
+
+# order
 d = d[order(d$CHR, d$POS), ]
-d <- d[, c("CHRCBP", "EA", "NEA", "EAF",  "EFFECT", "SE", "PVALUE", "N_INFORMATIVE")]
-d.tab = subset(d, d$PVALUE < 5e-8)
+
+# filter
+d.sig = subset(d, d$PVALUE < 5e-08)
+# d.tab = subset(d, d$PVALUE < 5e-8)
+
+# prettify
+d <- d[, c("CHRCBP", "CHR", "POS", "EA", "NEA", "EAF",  "EFFECT", "SE", "PVALUE", "N_INFORMATIVE")]
 
 # export
-write.table(d.all, file = file.all, col.names = T, row.names = F, quote = F, sep = "\t")
+write.table(d, file = file.all, col.names = T, row.names = F, quote = F, sep = "\t")
 write.table(d.sig, file = file.sig, col.names = T, row.names = F, quote = F, sep = "\t")
-write.table(d.tab, file = file.tab, col.names = T, row.names = F, quote = F, sep = "\t")
-
+# write.table(d.tab, file = file.tab, col.names = T, row.names = F, quote = F, sep = "\t")
